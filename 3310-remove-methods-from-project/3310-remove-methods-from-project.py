592065c4356d44666cc5cf32fbc22feba108c39b
Sudhanshu Kalekinge <74017676+26052007@users.noreply.github.com>
2026-08-05 09:56:02 +0530

    Time: 313 ms (62.12%), Space: 106.9 MB (59.85%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,24 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: list[list[int]]) -> list[int]:
+        from collections import defaultdict, deque
+
+        graph = defaultdict(list)
+        for u, v in invocations:
+            graph[u].append(v)
+
+        suspicious = set()
+        queue = deque([k])
+        suspicious.add(k)
+
+        while queue:
+            node = queue.popleft()
+            for neighbor in graph[node]:
+                if neighbor not in suspicious:
+                    suspicious.add(neighbor)
+                    queue.append(neighbor)
+
+        for u, v in invocations:
+            if u not in suspicious and v in suspicious:
+                return list(range(n))
+
+        return [i for i in range(n) if i not in suspicious]
